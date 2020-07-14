cca1c72893244f96608d4ccca2b195e42a199ee5
Ganesh Kathiresan <ganesh3597@gmail.com>
2020-07-14 19:18:28 +0530

    Added progess | Verbose

--- parse_tamil.py
+++ parse_tamil.py
@@ -1,27 +1,37 @@
 import xml.etree.ElementTree as etree
-import lxml.etree
-# p root.findall('m:page/m:revision/m:text', namespaces)[1].text
+import argparse
+from tqdm import tqdm
 
-NAMESPACES = {'m': 'http://www.mediawiki.org/xml/export-0.10/'}
+NAMESPACES = {'wiki': 'http://www.mediawiki.org/xml/export-0.10/'}
+TEXT_XPATH = 'wiki:page/wiki:revision/wiki:text'
 
 class TamilParser:
-    def __init__(self):
-        pass
+    def __init__(self, filepath):
+        self.filepath = filepath
 
     @staticmethod
     def is_tamil(word):
         return ord(word[0]) >= 2944 and ord(word[0]) <= 3071
 
-    def yield_text_from_file(filename):
-        root = etree.parse(filename).getroot()
-        for textnode in root.findall('m:page/m:revision/m:text', NAMESPACES):
+    def yield_text_from_file(self):
+        print('Parsing dump file. This may take 10 to 30 seconds depending on your machine...')
+        root = etree.parse(self.filepath).getroot()
+        for textnode in tqdm(root.findall(TEXT_XPATH, NAMESPACES), desc='Progess'):
             yield(textnode.text)
-        #for event, elem in etree.iterparse(filename, events=('start', 'end')):
-        #    if event == 'start':
-        #        import pdb; pdb.set_trace()
+
+    def parse_text(self):
+        for text in self.yield_text_from_file():
+            pass
 
 if __name__ == '__main__':
-    print(TamilParser.is_tamil("112i"))
-    print(TamilParser.is_tamil("தமிழ்"))
 
-    TamilParser.yield_text_from_file('tawiki-20200701-pages-meta-current.xml')
+    # print(TamilParser.is_tamil("தமிழ்"))
+
+    parser = argparse.ArgumentParser(description='Script to find unique tamil words in wikipedia corpus')
+    parser.add_argument('-f', '--filepath', dest='filepath', action='store', help='Path to wiki pages xml dump')
+
+    options = parser.parse_args()
+    # import pdb; pdb.set_trace()
+
+    tp = TamilParser(options.filepath)
+    tp.parse_text()
